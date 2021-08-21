5689aa01e131ace28eef5ee8a16465899a1713e5
sadhwani880@gmail.com <sadhwani880@gmail.com>
2021-08-21 20:51:37 +0530

    Updates

---
+++ precache-manifest.e9864979c4d0fbf3a3d25a84646374a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a5a3b02dce4e9d188ad3ef38e55dbd9",
+    "url": "/priyasadhwani/contact-book.git/index.html"
+  },
+  {
+    "revision": "385f03227a41388a06f7",
+    "url": "/priyasadhwani/contact-book.git/static/css/main.5d6b07b6.chunk.css"
+  },
+  {
+    "revision": "34a481b847e3224394ff",
+    "url": "/priyasadhwani/contact-book.git/static/js/2.644ef2d2.chunk.js"
+  },
+  {
+    "revision": "d0c7295dade8792d4ec795e8fb19eb02",
+    "url": "/priyasadhwani/contact-book.git/static/js/2.644ef2d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "385f03227a41388a06f7",
+    "url": "/priyasadhwani/contact-book.git/static/js/main.ff0ad723.chunk.js"
+  },
+  {
+    "revision": "69b0ad2dae98f578ee81",
+    "url": "/priyasadhwani/contact-book.git/static/js/runtime-main.54ffd7ea.js"
+  }
+]);
